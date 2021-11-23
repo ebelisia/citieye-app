15f42302d64052b71f3e52204a20b69423aaec90
ebelisia <eduardo.belisia@gmail.com>
2021-11-23 15:03:18 -0300

    changing port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3001 | process.env.PORT;
+const port = 5000 | process.env.PORT;
 
 app.use(express.static(__dirname + '/public'));
 
