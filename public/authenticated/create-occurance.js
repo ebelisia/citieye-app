098d781d81aca498d9d463ccbbdb1adb53d162f1
edubelisia <eduardo.belisia@mercadolivre.com>
2021-11-23 14:32:36 -0300

    first commit

--- public/authenticated/create-occurance.js
+++ public/authenticated/create-occurance.js
@@ -1,31 +1,36 @@
 $("form").on("submit", (e) => {
     e.preventDefault();
     const usuario = JSON.parse(localStorage.getItem('user'))
-    let $user = usuario.userName;
+    // let $user = usuario.userName;
     let $title = $("#title").val();
-    let $place = $("#locale").val();
+    let $local = $("#locale").val();
     let $address = $("#address").val();
     let $description = $("#description").val();
     let $category = $("#category :selected").data("id");
+    let $city = $("#city :selected").data("id");
 
     //Cria o objeto de categoria
     let categoryObject = {
         id: $category
     }
 
+    let cityObject = {
+        id: $city
+    }
+
     let postObject = {
-        author: $user,
         title: $title,
-        profile_picture: "https://www.shareicon.net/data/512x512/2017/04/19/884537_blue_512x512.png",
+        // profile_picture: "https://www.shareicon.net/data/512x512/2017/04/19/884537_blue_512x512.png",
         address: $address,
-        place: $place,
+        local: $local,
         category: categoryObject,
-        description: $description
+        description: $description,
+        city: cityObject
     }
 
     $.ajax({
         type: "POST",
-        url: "http://localhost:8080/occurrences",
+        url: "https://citieye.herokuapp.com/occurrences?userId=1",
         contentType: "application/json; charset=utf-8",
         dataType: 'json',
         data: JSON.stringify(postObject),
@@ -50,18 +55,31 @@ $("form").on("submit", (e) => {
     })
 })
 
-
 let categoryList = $("#category");
+let cityList = $("#city");
 
 $.ajax({
-    url: "http://localhost:8080/categories",
+    url: "https://citieye.herokuapp.com/categories",
     method: "GET",
     header: {
         "Content-Type": "application/json"
     },
     success: function (response) {
         response.forEach(item => {
-            categoryList.append(`<option value="${item.description}" data-id="${item.id}">${item.description}</option>`)
+            categoryList.append(`<option value="${item.name}" data-id="${item.id}">${item.name}</option>`)
+        })
+    }
+})
+
+$.ajax({
+    url: "https://citieye.herokuapp.com/cities",
+    method: "GET",
+    header: {
+        "Content-Type": "application/json"
+    },
+    success: function (response) {
+        response.forEach(item => {
+            cityList.append(`<option value="${item.name}" data-id="${item.id}">${item.name}</option>`)
         })
     }
 })
--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
 const invoincingAPI = () => {
     $.ajax({
         //Mudar para localhost:3000/posts caso for utilizar o json-server
-        url: "http://localhost:8080/occurrences",
+        url: "https://citieye.herokuapp.com/occurrences/all",
         method: "GET",
         header: {
             "Content-Type": "application/json"
@@ -11,7 +11,7 @@ const invoincingAPI = () => {
             setTimeout(() => {
                 $(".loading").remove();
                 mountElements(response);
-            }, 2500);
+            }, 1500);
 
             if (!response.length) {
                 return;
@@ -32,45 +32,41 @@ const invoincingAPI = () => {
 
 invoincingAPI();
 
-const mountDeleteButton = (user_local, owner_post) => {
-    if (user_local == owner_post) {
-        return `<button class="delete-post"></button>`
-    } else {
-        return "";
-    }
-}
+// const mountDeleteButton = (user_local, owner_post) => {
+//     if (user_local == owner_post) {
+//         return `<button class="delete-post"></button>`
+//     } else {
+//         return "";
+//     }
+// }
 
 //Montas os elementos em tela de acordo com os dados fornecidos pela API
 const mountElements = (props) => {
     let $postsDiv = $(".posts");
-    var jsonUser = localStorage.getItem('user');
-    var userLocalStorage = "" || JSON.parse(jsonUser);
-
-
+    // var jsonUser = localStorage.getItem('user');
+    // var userLocalStorage = "" || JSON.parse(jsonUser);
     props.forEach(item => {
 
         //Obtendo as informações vindas de "item" por meio de desestruturação
-        const { id, profile_picture, author, user_district, title, place, address, description, category } = item;
-
+        const { id, user, title, local, address, description, category, city } = item;
+        console.log(item);
         $postsDiv.append(`
             <article class="post" data-id="${id}">
-            ${jsonUser != null ? mountDeleteButton(userLocalStorage.userName, author) : ""}
             <button class="support"></button>
             <button class="share"></button>
                 <div class="user-information">
-                    <img src=${profile_picture} alt="foto de perfil">
+                    <img src="https://s.conjur.com.br/img/b/joaquim-barbosa6.png" alt="foto de perfil">
                     <div>
-                        <span class="name">${author !== "" ? author : "Usuário Anônimo"}</span>
-                        <span class="live-in"> ${user_district !== undefined ? `Morador de ${author}` : "Bairro onde reside não informado"}</span>
-                        <span class="live-in"> Categoria: ${category.description}</span>
-
+                        <span class="name">${user !== "" ? user.name + " " + user.surname : "Usuário Anônimo"}</span>
+                        <span class="live-in"> Categoria: ${category.name}</span>
                     </div>
                 </div>
 
                 <h3 class="occurrance-title">
                     ${title}
                 </h3>
-                <p class="place">Local: ${place}</p>
+                <p class="place">Local: ${local}</p>
+                <p class="city">Cidade: ${city.name}</p>
                 <p class="place">Endereço: ${address !== "" ? ` ${address}` : "Não informado"}</p>
                 <span>
                     ${description}
@@ -78,7 +74,6 @@ const mountElements = (props) => {
             </article>
             `)
     })
-
 
     $(".delete-post").on("click", function () {
         let id = $(this).parent().data("id");
@@ -94,7 +89,7 @@ const mountElements = (props) => {
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajax({
-                    url: "http://localhost:8080/occurrences/" + id,
+                    url: "https://citieye.herokuapp.com/occurrences/" + id,
                     method: "DELETE",
                     success: function () {
                         Swal.fire({
@@ -110,7 +105,7 @@ const mountElements = (props) => {
     })
 
     $(".support").on("click", function(){
-        alert('APOIO')
+        alert('APOIO!')
     })
 
     $(".share").on("click", () => {
